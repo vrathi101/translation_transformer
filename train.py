05c207d980e5d9000172a1175d67a9fd60f9f5f7
Vedant Rathi <72925608+vrathi101@users.noreply.github.com>
2025-01-18 15:34:50 -0600

    Update train.py

--- train.py
+++ train.py
@@ -123,13 +123,13 @@ def get_all_sentences(ds, lang):
     for item in ds:
         yield item['translation'][lang]
 
-def get_or_build_tokenizer(config, ds, lang):
-    tokenizer_path = Path(config['tokenizer_file'].format(lang))
+def get_or_build_tokenizer(config, ds, lang1, lang2):
+    tokenizer_path = Path(config['tokenizer_file'].format(lang1, lang2))
     if not Path.exists(tokenizer_path):
         tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
         tokenizer.pre_tokenizer = Whitespace()
         trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
-        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang1), trainer=trainer)
         tokenizer.save(str(tokenizer_path))
     else:
         tokenizer = Tokenizer.from_file(str(tokenizer_path))
@@ -140,8 +140,8 @@ def get_ds(config):
     ds_raw = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
 
     # Build tokenizers
-    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
-    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'], config['lang_tgt'])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'], config['lang_src'])
 
     # Keep 90% for training, 10% for validation
     train_ds_size = int(0.9 * len(ds_raw))
